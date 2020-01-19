19476ec50ff30bb093127d5306d7f9ad1ecdc9cb
hwanggit <luw055@ucsd.edu>
2020-01-19 13:54:46 -0800

    Completed initial keyboard and view animations

--- Quick Tip/AppDelegate.swift
+++ Quick Tip/AppDelegate.swift
@@ -11,7 +11,16 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var blockRotation: Bool = false
 
+    // Block rotation
+    private func application(application: UIApplication, supportedInterfaceOrientationsForWindow window: UIWindow?) -> Int {
+        if (self.blockRotation) {
+            return Int(UIInterfaceOrientationMask.portrait.rawValue)
+        } else {
+            return Int(UIInterfaceOrientationMask.all.rawValue)
+        }
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- Quick Tip/ViewController.swift
+++ Quick Tip/ViewController.swift
@@ -8,13 +8,156 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIApplicationDelegate {
+    
+    var appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+    @IBOutlet weak var priceInput: UITextField!
+    @IBOutlet weak var tipInput: UITextField!
+    @IBOutlet weak var equalsSign: UILabel!
+    @IBOutlet weak var plusSign: UILabel!
+    @IBOutlet weak var postTip: UILabel!
+    @IBOutlet weak var tipMode: UISegmentedControl!
+    @IBOutlet weak var tipView: UIView!
+    
+    var tipModePercentage: NSNumber?
+    let screenSize:CGRect = UIScreen.main.bounds
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        appDelegate.blockRotation = true
+
+        // Initialize input box style
+        priceInput.keyboardType = .numberPad
+        priceInput.attributedPlaceholder = NSAttributedString(string: "$",
+        attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        
+        // Initialize input box style
+        tipInput.keyboardType = .numberPad
+
+        // Put view offset on right
+        let rightView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 10, height: tipInput.frame.height))
+        tipInput.rightView  = rightView
+        tipInput.rightViewMode = .always
+
+        // Disable user interaction if not custom
+        if tipMode.selectedSegmentIndex != 3 {
+            tipInput.isUserInteractionEnabled = false
+        }
+
+        // Initialize tap recognizer, and add to view
+        let tapScreen = UITapGestureRecognizer(target: self, action: #selector(didTapScreen(sender:)))
+        
+        let tapPrice = UITapGestureRecognizer(target: self, action: #selector(didTapPrice(sender:)))
+        
+        let tapTip = UITapGestureRecognizer(target: self, action: #selector(didTapTip(sender:)))
+        
+        // Add tap handlers to view
+        view.addGestureRecognizer(tapScreen)
+        priceInput.addGestureRecognizer(tapPrice)
+        tipInput.addGestureRecognizer(tapTip)
+        
+        // Listen to keyboard open
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        // Listen for text field input change
+        priceInput.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        
+        // Hide options display and calculations
+        tipView.isHidden = true
+        
+        // Hide mode segment
+        tipMode.isHidden = true
+    }
+    
+    // Change mode based on segment
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        switch tipMode.selectedSegmentIndex {
+        case 0:
+            tipModePercentage = 0
+            tipInput.isUserInteractionEnabled = false
+            tipInput.attributedPlaceholder = NSAttributedString(string: "",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+            
+        case 1:
+            tipModePercentage = 10
+            tipInput.isUserInteractionEnabled = false
+            tipInput.attributedPlaceholder = NSAttributedString(string: "",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+            
+        case 2:
+            tipModePercentage = 15
+            tipInput.isUserInteractionEnabled = false
+            tipInput.attributedPlaceholder = NSAttributedString(string: "",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+            
+        default:
+            tipModePercentage = nil
+            tipInput.isUserInteractionEnabled = true
+            tipInput.becomeFirstResponder()
+            tipInput.layer.cornerRadius = 10.0
+            tipInput.attributedPlaceholder = NSAttributedString(string: "$",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        }
+    }
+    
+    // Close keyboard on tap
+    @objc func didTapScreen(sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+        
+        // Make view return to normal
+        view.frame = CGRect(x:0, y:0, width: screenSize.width, height: screenSize.height)
+    }
+    
+    // Make view height shrink on tap
+    @objc func didTapPrice(sender: UITapGestureRecognizer) {
+        // Open keyboard
+        priceInput.becomeFirstResponder()
+    }
+    
+    // Make view height shrink on tap
+    @objc func didTapTip(sender: UITapGestureRecognizer) {
+        // Open keyboard
+        tipInput.becomeFirstResponder()
+    }
+    
+    // Set the shouldAutorotate to False
+    override open var shouldAutorotate: Bool {
+       return false
     }
 
-
+    // Specify the orientation.
+    override open var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+       return .portrait
+    }
+    
+    // Handle keyboard showing method
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            
+            // Adjust view size
+            view.frame = CGRect(x:0, y:0, width: screenSize.width, height: screenSize.height - keyboardHeight)
+        }
+    }
+    
+    // When textfield changes, hide/show options view
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        // Check if text field empty or not
+        guard let text = textField.text, !text.isEmpty else {
+            tipMode.isHidden = false
+            tipView.isHidden = false
+            return
+        }
+        
+        tipMode.isHidden = true
+        tipView.isHidden = true
+    }
 }
 
