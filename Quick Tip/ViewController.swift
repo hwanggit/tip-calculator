a3005e96a63ca2a34c0db35272747a6efb97ebd0
hwanggit <luw055@ucsd.edu>
2020-01-18 17:13:17 -0800

    Initial Commit

---
+++ Quick Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quick Tip
+//
+//  Created by Luhao Wang on 1/18/20.
+//  Copyright © 2020 Luhao Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
